f593928a3525be0f4b8b7a7348b5f065d4c5b5ca
FSD <91106755+Jasmine409@users.noreply.github.com>
2023-05-02 00:59:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employee.cu7jlsmasnsk.us-east-1.rds.amazonaws.com"
 customuser = "fsd"
 custompass = "2102939.db"
-customdb = "employee"
+customdb = "EmployeeDB"
 custombucket = "fongsukdien-bucket"
 customregion = "us-east-1"
